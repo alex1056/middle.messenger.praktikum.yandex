175a4006a4b9c4d6c8db853020720dfdf06bf975
Alexander <gituser1056@gmail.com>
2021-03-23 18:14:24 +0300

    Верстка страницы chat-2

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// import './css/style.css';
+// const pug = require('pug');
+// import { pug } from 'pug';
+
+// import { sum } from '../src/modules/sum';
+const preTemplate = require('./templates');
+// import preTemplate from './templates';
+
+console.log(preTemplate);
+// const compiledFunction = require('../src/templates');
+
+// const root = document.querySelector('#root');
+// // root.textContent = sum(9, -1).toString();
+// const newText = document.createElement('b');
+// newText.innerHTML = 'bla-bal';
+// root.appendChild(newText);
+// root.appendChild(document.createElement(sum(9, -1).toString()));
+
+// console.log(
+//   compiledFunction({
+//     name: 'Timothy',
+//   })
+// );
