b5c137225779db76f1e8001379a4273eb97b13b2
Alexander <gituser1056@gmail.com>
2021-04-16 19:56:18 +0300

    Test добавил еще один login-form1

---
+++ src/components/login-form/login-form1.ts
@@ -0,0 +1,31 @@
+import { Block } from '../Block';
+import { Btn } from '../Button';
+import { tmplLogin } from './template';
+import './style.scss';
+
+const pug = require('pug');
+
+type TProps = { [propName: string]: any };
+
+export class LoginForm extends Block {
+  props: TProps;
+
+  constructor(props?: TProps) {
+    super('div', {
+      buttonsubmit: new Btn({
+        ...props,
+        buttonText: 'Аворизоваться',
+        className: 'btn_disabled',
+        disabled: true,
+      }),
+    });
+  }
+
+  render(): string {
+    const compiled = pug.compile(tmplLogin);
+    const html = compiled({
+      buttonsubmit: this.props.buttonsubmit.render(),
+    });
+    return html;
+  }
+}
--- src/pages/add-user.ts
+++ src/pages/add-user.ts
@@ -5,15 +5,5 @@ const popupAddUser = new PopupAddUser({
   // className: "pform__btn-save btn_hide",
   // disabledAttr: "disabled",
 });
-// const form = new Form();
+
 renderDOM('.page', popupAddUser.getContent());
-
-// form.setPopup(document.body as HTMLDivElement);
-// form.setEventListeners();
-// const currentForm = document.body.querySelector("#form") as HTMLFormElement;
-
-// const formValidator = new Validator(currentForm);
-// formValidator.setHandleLabels(true);
-// // console.log(formValidator);
-// form.setFormValidator(formValidator as any);
-// form.setHandlers("submit", onSubmitHandlerLogin);
