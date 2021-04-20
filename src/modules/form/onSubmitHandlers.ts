7a32e8c66656dfd16ed6e13ba20452f7e95331fd
Alexander <gituser1056@gmail.com>
2021-04-20 16:12:33 +0300

    Все правки внесены

--- src/modules/form/onSubmitHandlers.ts
+++ src/modules/form/onSubmitHandlers.ts
@@ -2,13 +2,12 @@ export function onSubmitHandlerLogin(form: HTMLFormElement) {
   const inputs = Array.from(form.elements);
   const submit = form.querySelector('#submit') as HTMLDivElement;
 
-  const inputsData = inputs.reduce((acc: object, item: HTMLInputElement) => {
-    const { id, value } = item;
+  const inputsData = inputs.reduce((acc: object, { id, value }: HTMLInputElement) => {
     if (id && id !== submit.id) {
       return { ...acc, [id]: value };
     }
     return acc;
   }, {});
-  console.log(inputsData);
+
   return inputsData;
 }
