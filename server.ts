ca420f01f02f66cd8fd23adb57c7d918c9f0651a
Alexander <gituser1056@gmail.com>
2021-06-03 20:15:49 +0300

    Из process.env берем PORT и HOSTNAME

--- server.ts
+++ server.ts
@@ -3,8 +3,8 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
-const HOSTNAME = '0.0.0.0';
+const HOSTNAME = process.env.HOSTNAME || '0.0.0.0';
+const PORT = process.env.PORT || 3000;
 const distFolder = `${__dirname}/dist/`;
 
 app.use(express.static(distFolder));
