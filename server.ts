f13ba7549d430fffecef0f3256fdbdf46e572e2e
Alexander <gituser1056@gmail.com>
2021-06-03 19:29:09 +0300

    Добавлен Docker

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ const path = require('path');
 
 const app = express();
 const PORT = 3000;
+const HOSTNAME = '0.0.0.0';
 const distFolder = `${__dirname}/dist/`;
 
 app.use(express.static(distFolder));
@@ -12,6 +13,6 @@ app.use((request, response) => {
   response.sendFile(path.resolve(`${distFolder}index.html`));
 });
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+app.listen(PORT, HOSTNAME, () => {
+  console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
 });
