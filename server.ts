02f0d6fb15302150e91311001fd228f944a8aab9
Alexander <gituser1056@gmail.com>
2021-06-03 18:01:41 +0300

    Починили reload

--- server.ts
+++ server.ts
@@ -4,15 +4,12 @@ const path = require('path');
 
 const app = express();
 const PORT = 3000;
-// const distFolder = `${__dirname}/dist/`;
-// app.use(express.static(distFolder));
-// app.get('*', (request, response) => {
-//   response.sendFile(path.resolve(`${distFolder}index.html`));
-// });
-app.use(express.static('./dist'));
+const distFolder = `${__dirname}/dist/`;
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+app.use(express.static(distFolder));
+
+app.use((request, response) => {
+  response.sendFile(path.resolve(`${distFolder}index.html`));
 });
 
 app.listen(PORT, () => {
